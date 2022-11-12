307655ccb0d00d7c64b50459ec43ee3a750efc38
MurgrayDJ <murgraydj@gmail.com>
2022-11-11 19:00:13 -0500

    Creates and calls run program function

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,3 +2,9 @@
 # Date created: 11/11/2022 
 # Purpose: Uses bubble sort to sort an array
 
+def run_program
+    unsorted_array = [4,3,78,2,0,2]
+    bubble_sort(unsorted_array)
+end
+
+run_program
